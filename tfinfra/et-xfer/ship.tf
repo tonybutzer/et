33773bd9cbe1b9e7ea84a01fad2429e5a1c510cf
tonybutzer <tonybutzer@gmail.com>
2020-02-26 00:33:27 -0500

    automatic git update from Makefile

--- tfinfra/et-xfer/ship.tf
+++ tfinfra/et-xfer/ship.tf
@@ -6,18 +6,18 @@ provider "aws" {
 }
 
 
-resource "aws_instance" "et-webserver1" {
+resource "aws_instance" "et-xfer1" {
 
   ami           = "${var.ami}"
 
   instance_type = "${var.instance_type}"
   key_name                    = "${var.key_name}"
   tags {
-    Name = "et-webserver1"
+    Name = "et-xfer1"
     Owner = "tonybutzer@gmail.com"
   }
   iam_instance_profile                    ="${var.iam_role}"
   security_groups = ["${var.security_group}"]
   user_data                   = "${file("files/os_boot.sh")}"
-  root_block_device {volume_size = 15}
+  root_block_device {volume_size = 90}
 }
