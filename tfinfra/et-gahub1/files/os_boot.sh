732263786c53c63ac840c546021411a04e849427
tonybutzer <tonybutzer@gmail.com>
2020-02-26 10:52:25 -0500

    automatic git update from Makefile

---
+++ tfinfra/et-gahub1/files/os_boot.sh
@@ -0,0 +1,15 @@
+#! /bin/bash
+
+echo "Hello Tony" > /tmp/hellotony.txt
+
+hname=gahub1
+
+sudo hostname $hname
+echo "127.0.0.1 $hname" >> /etc/hosts
+sudo mkdir -p /opt
+
+(cd /opt; git clone http://github.com/tonybutzer/et)
+
+sudo chown -R ubuntu /opt
+
+# (cd /opt/et//pkg/os; ./setup_os.sh)
